2e725afb1d3845042df2910851d54e6620ee1a09
Rory Jackson <roryjackson@Rorys-MacBook-Air.local>
2023-01-16 16:53:13 +0000

    added layout files, with basic header html

---
+++ components/header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <nav>
+            <div>
+                <div className="header-logo">
+
+                </div>
+                <ul className="header-items">
+                    <li>Home</li>
+                    <li>The Design</li>
+                    <li>Gallery</li>
+                    <li>Pricing</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div>
+                <div class="container">
+                    <h1>ADVENTURE YURTS</h1>
+                    <p>We build CAD designed yurts using commercial machinery.</p>
+                    <div>
+                        <Link>Product</Link>
+                        <Link>Contact</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ components/layout.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;

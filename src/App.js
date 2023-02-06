80974d2f96d40920e74637313c9df2282581151d
rory jackson <roryjamesjackson@hotmail.com>
2023-02-06 10:41:40 -0800

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Layout from './components/layout';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ContactForm from './components/contactForm';
 
 function App() {
   return (
     <div className="App">
       <Layout />
+      <ContactForm/>
     </div>
   );
 }

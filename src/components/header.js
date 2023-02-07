d3dfc4a879578ae4cf7ecf63ed7143577ebe1af8
rory jackson <roryjamesjackson@hotmail.com>
2023-02-06 19:09:04 -0800

    completed design section css

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,28 @@
 // import Link from 'next/link';
+import '../styles/components/Header.css';
+import '../styles/Globals.css';
 
 const Header = () => {
     return (
         <nav>
-            <div>
+            <div className="header-container">
                 <div className="header-logo">
+                  <p>Adventure Yurts</p>
                 </div>
                 <ul className="header-items">
                     <li>Home</li>
                     <li>The Design</li>
                     <li>Gallery</li>
                     <li>Pricing</li>
-                    <li>Contact!</li>
+                    <li>Contact</li>
                 </ul>
             </div>
-            <div>
-                <div class="container">
+            <div class="title-container">
                     <h1>ADVENTURE YURTS</h1>
-                    <p>We build CAD designed yurts using commercial machinery.</p>
-                    <div>
+                    <p id="slogan">We build CAD designed yurts using commercial machinery.</p>
+                    {/* <div>
                         <a href="/">Find out more</a>
-                    </div>
-                </div>
+                    </div> */}
             </div>
         </nav>
     )
--- src/components/main.js
+++ src/components/main.js
@@ -1,26 +1,31 @@
-import ControlledCarousel from './gallery'
+import Design from './design';
+import ControlledCarousel from './gallery';
+import '../styles/components/Main.css';
 
 const main = () => {
   return (
     <div>
-      <div>
-        <div>
-          <img scr="" alt=""></img>
-        </div>
-        <div>
-          <h4>1</h4>
-          <h4>2</h4>
-          <h4>3</h4>
-          <h4>4</h4>
-          <h4>5</h4>
-        </div>
-        <div>
-          <h4>Read more...</h4>
-        </div>
-        <div>
-          <img scr="" alt=""></img>
+      <div class="section1">
+        <div class="parent">
+          <div class="div1">
+            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1675731943/pexels-geoff-duke-365717_hftrjq.jpg" alt=""></img>
+          </div>
+          <div class="div2">
+            <h4>Cool in summer, warm in winter</h4>
+            <h4>Family enterprise</h4>
+            <h4>CAD designed</h4>
+            <h4>Fully customizable</h4>
+            <h4>Amazing product</h4>
+          </div>
+          <div class="div3">
+            <h4>Read more...</h4>
+          </div>
+          <div class="div4">
+            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1662388365/cld-sample-2.jpg" alt=""></img>
+          </div>
         </div>
       </div>
+      < Design/>
       <div>
         <h3>Prices start from £12'999</h3>
       </div>

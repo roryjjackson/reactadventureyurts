5455f35b0d5db7891bcab3a7004516580b23fb9a
rory jackson <roryjamesjackson@hotmail.com>
2023-02-10 09:31:44 -0800

    basic styling done

--- src/components/main.js
+++ src/components/main.js
@@ -6,22 +6,29 @@ const main = () => {
   return (
     <div>
       <div class="section1">
-        <div class="parent">
+        <div class="top-section">
           <div class="div1">
             <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1675731943/pexels-geoff-duke-365717_hftrjq.jpg" alt=""></img>
           </div>
           <div class="div2">
-            <h4>Cool in summer, warm in winter</h4>
-            <h4>Family enterprise</h4>
-            <h4>CAD designed</h4>
-            <h4>Fully customizable</h4>
-            <h4>Amazing product</h4>
+            <div>
+              <div id="upper-text">
+                <h4>Cool in summer, warm in winter</h4>
+                <p>Family enterprise and some other text</p>
+              </div>
+              <h4 id="mid-text">CAD DESIGNED</h4>
+              <div id="lower-text">
+                <p>Fully customizable</p>
+                <h3>Amazing product</h3>
+              </div>
+            </div>
           </div>
+        </div>
+        <div class="bottom-section">
           <div class="div3">
-            <h4>Read more...</h4>
           </div>
           <div class="div4">
-            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1662388365/cld-sample-2.jpg" alt=""></img>
+            <h4>Read more!</h4>
           </div>
         </div>
       </div>

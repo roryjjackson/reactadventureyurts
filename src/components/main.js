820caa7a3304ac5bb7b482d3eef93d1a88a2f806
rory jackson <roryjamesjackson@hotmail.com>
2023-02-13 12:20:58 -0800

    completed design of about section with icons and sizing

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,8 @@
 import Design from './design';
 import ControlledCarousel from './gallery';
 import '../styles/components/Main.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const main = () => {
   return (
@@ -8,17 +10,18 @@ const main = () => {
       <div class="section1">
         <div class="top-section">
           <div class="div1">
-            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1675731943/pexels-geoff-duke-365717_hftrjq.jpg" alt=""></img>
+            <h3>Designed and built in the North East, UK.</h3>
+            <p>< FontAwesomeIcon icon={faLocationDot} id="location-dot"></FontAwesomeIcon>Millenium bridge, River Tyne</p>
           </div>
           <div class="div2">
             <div>
               <div id="upper-text">
-                <h4>Cool in summer, warm in winter</h4>
-                <p>Family enterprise and some other text</p>
+                <h4>Perfectly round,</h4>
+                <p>CAD designed wooden yurt</p>
               </div>
-              <h4 id="mid-text">CAD DESIGNED</h4>
+              <h4 id="mid-text">FAMILY RUN BUSINESS</h4>
               <div id="lower-text">
-                <p>Fully customizable</p>
+                <p>Fully customizable and some more text here</p>
                 <h3>Amazing product</h3>
               </div>
             </div>
@@ -29,6 +32,7 @@ const main = () => {
           </div>
           <div class="div4">
             <h4>Read more! </h4>
+            < FontAwesomeIcon icon={faArrowRight} id="right-arrow"></FontAwesomeIcon>
           </div>
         </div>
       </div>

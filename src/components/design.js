8c05738f64f6da3ba5195ebe67c1901fde1898a5
rory jackson <roryjamesjackson@hotmail.com>
2023-02-13 16:09:45 -0800

    added number icons to list

--- src/components/design.js
+++ src/components/design.js
@@ -1,33 +1,37 @@
 import '../styles/components/Design.css';
 import '../styles/Globals.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa1, fa2, fa3, fa4 } from '@fortawesome/free-solid-svg-icons';
+
 
 const Design = () => {
   return (
     <div class="design-container">
       <div class="design-list">
         <div class="design-list-item">
-            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt="pic of a tent"></img>
+        < FontAwesomeIcon icon={fa1} class="number-icon"></FontAwesomeIcon>
             <div class="design-list-info">
+
               <h3>Stage One</h3>
               <p>This happens</p>
             </div>
         </div>
         <div class="design-list-item">
-            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt="pic of a tent"></img>
+        < FontAwesomeIcon icon={fa2} class="number-icon"></FontAwesomeIcon>
             <div class="design-list-info">
               <h3>Stage Two</h3>
               <p>This happens</p>
             </div>
         </div>
         <div class="design-list-item">
-            <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt="pic of a tent"></img>
+        < FontAwesomeIcon icon={fa3} class="number-icon"></FontAwesomeIcon>
             <div class="design-list-info">
               <h3>Stage Three</h3>
               <p>This happens</p>
             </div>
         </div>
         <div class="design-list-item">
-          <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt="pic of a tent"></img>
+        < FontAwesomeIcon icon={fa1} class="number-icon"></FontAwesomeIcon>
           <div class="design-list-info">
             <h3>Stage Four</h3>
             <p>This happens</p>

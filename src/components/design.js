37f7311c7347c0acce11bb2ca83e0d02242fc39d
rory jackson <roryjamesjackson@hotmail.com>
2023-02-13 16:47:07 -0800

    css styling

--- src/components/design.js
+++ src/components/design.js
@@ -11,7 +11,6 @@ const Design = () => {
         <div class="design-list-item">
         < FontAwesomeIcon icon={fa1} class="number-icon"></FontAwesomeIcon>
             <div class="design-list-info">
-
               <h3>Stage One</h3>
               <p>This happens</p>
             </div>
@@ -31,15 +30,28 @@ const Design = () => {
             </div>
         </div>
         <div class="design-list-item">
-        < FontAwesomeIcon icon={fa1} class="number-icon"></FontAwesomeIcon>
+        < FontAwesomeIcon icon={fa4} class="number-icon"></FontAwesomeIcon>
           <div class="design-list-info">
             <h3>Stage Four</h3>
             <p>This happens</p>
           </div>
         </div>
       </div>
-      <div class="design-img">
-        <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669386894/pexels-diana-vorobevaattyakova-8972946_xmaxwo.jpg" alt="pic of a tent"></img>
+      <div class="design-info">
+        <div class="design-header">
+          <h4>Overview</h4>
+          <h4>Gallery</h4>
+          <h4>Maps</h4>
+        </div>
+        <div class="design-data">
+          <h5>This is something lovely</h5>
+          <br/>
+          <h6>some more text here about something</h6>
+        </div>
+        <div class="design-links">
+          {/* <a href="/">A button</a> */}
+          {/* <img src="/" alt="/"></img> */}
+        </div>
       </div>
     </div>
   )
